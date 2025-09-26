2c71412e0f1f8fc9ea7d2c3fb1c1824a80711e10
SidharthBSunil <91207543+SidharthBSunil@users.noreply.github.com>
2025-09-26 07:07:17 +0530

    esp32

---
+++ espiot2.ino
@@ -0,0 +1,39 @@
+#define BLYNK_TEMPLATE_ID "TMPL3A9hngqcD"
+#define BLYNK_TEMPLATE_NAME "distance"
+#define BLYNK_AUTH_TOKEN "eiFBtCo5dkdZAtj3XsAuZm7XB3bLLjrd"
+
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+// Your WiFi credentials
+char ssid[] = "Your_WiFi_Name";     // change with your WiFi SSID
+char pass[] = "Your_WiFi_Password"; // change with your WiFi Password
+
+// Pin for LED
+#define LED_PIN 12
+
+BLYNK_WRITE(V1) // When Virtual Pin V1 on Blynk app changes
+{
+  int pinValue = param.asInt(); // Get value from Blynk app (0 or 1)
+  digitalWrite(LED_PIN, pinValue); 
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW); // Keep LED OFF initially
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+}
